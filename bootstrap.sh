eaa0ede2650d0d969085b28b78a408e5b020363b
Austin Sims <adsims2001@gmail.com>
2015-03-20 18:58:00 -0400

    add samba to bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 apt-get update
-apt-get install -y git nodejs-legacy npm redis-server
+apt-get install -y git nodejs-legacy npm redis-server samba
 npm install -g grunt-cli bower node-inspector
 
